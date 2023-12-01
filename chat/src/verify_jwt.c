63b8cdaeb9669c05b158bc2df61e6e849b4e6232
hokuma <ho7221@gmail.com>
2023-12-01 14:38:50 +0900

    intended vuln #1 patched

--- chat/src/verify_jwt.c
+++ chat/src/verify_jwt.c
@@ -5,7 +5,7 @@ int verify_jwt(const char *jwt_string, const char *username) {
     jwt_t *jwt = NULL;
 
     char *key = strdup(SECRET_KEY);
-    int ret = jwt_decode(&jwt, jwt_string, NULL, 0);
+    int ret = jwt_decode(&jwt, jwt_string, SECRET_KEY, 0);
 
     if (ret != 0) {
         fprintf(stderr, "Invalid JWT.\n");
